338b02ece1ee207aed71e800641a0135e49e4639
pierluigi <pierlucolombo@gmail.com>
2021-01-17 20:16:45 +0100

    addedd some notes

--- main.py
+++ main.py
@@ -29,21 +29,38 @@ model = torch.nn.Sequential(torch.nn.Linear(784, 128),
                       torch.nn.LogSoftmax(dim=1))
 
 
-criterion = torch.nn.NLLLoss()
+
 
 '''
 another possible implementation would be:
-model = nn.Sequential(nn.Linear(784, 128),
-                      nn.ReLU(),
-                      nn.Linear(128, 64),
-                      nn.ReLU(),
-                      nn.Linear(64, 10))
-criterion = nn.CrossEntropyLoss()
-infact nn.CrossEntropy combines nn.LogSoftmax() and nn.NLLLoss() in one single class.
-The input is expected to contain scores for each class.
-'''
+class Classifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 10)
+        
+    def forward(self, x):
+        # make sure input tensor is flattened
+        x = x.view(x.shape[0], -1)
+        
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.log_softmax(self.fc4(x), dim=1)
+        
+        return x
 
+model = Classifier()
+'''
+criterion = torch.nn.NLLLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.003)
+
+'''
+another optimizer
+optimizer = optim.Adam(model.parameters(), lr=0.003)
+'''
 
 epochs = 5
 for e in range(epochs):
