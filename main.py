96abcbf5e3507c930fb0359fae99c32a828da931
pierluigi <pierlucolombo@gmail.com>
2021-01-18 18:16:42 +0100

    modified the main.py file
    the main now utilizes the networks in the network file and the train function in the utility file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import torch
 from torchvision import datasets, transforms
 import os
+from networks import Classifier_DropOut, Classifier_noDropOut
+from utility import train_model
 
 
 # Define a transform to normalize the data
@@ -20,40 +22,8 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 testset = datasets.FashionMNIST(path + 'input/testset/', download=True, train=False, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
 
+model = Classifier_noDropOut()
 
-model = torch.nn.Sequential(torch.nn.Linear(784, 128),
-                      torch.nn.ReLU(),
-                      torch.nn.Linear(128, 64),
-                      torch.nn.ReLU(),
-                      torch.nn.Linear(64, 10),
-                      torch.nn.LogSoftmax(dim=1))
-
-
-
-
-'''
-another possible implementation would be:
-class Classifier(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(784, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 64)
-        self.fc4 = nn.Linear(64, 10)
-        
-    def forward(self, x):
-        # make sure input tensor is flattened
-        x = x.view(x.shape[0], -1)
-        
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.log_softmax(self.fc4(x), dim=1)
-        
-        return x
-
-model = Classifier()
-'''
 criterion = torch.nn.NLLLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.003)
 
@@ -62,49 +32,15 @@ another optimizer
 optimizer = optim.Adam(model.parameters(), lr=0.003)
 '''
 
-epochs = 5
-for e in range(epochs):
-    running_loss = 0
-    for images, labels in trainloader:
-        # Flatten MNIST images into a 784 long vector
-        images = images.view(images.shape[0], -1)
-    
-        # TODO: Training pass
-        optimizer.zero_grad()
-        
-        output = model.forward(images)
-        loss = criterion(output, labels)
-        loss.backward()
-        optimizer.step()
-        
-        running_loss += loss.item()
-    else:
-        print(f"Training loss: {running_loss/len(trainloader)}")
+epochs = 200
 
+train_losses_ndp, test_losses_ndp, test_accuracy_ndp = train_model(model, optimizer, criterion, trainloader, testloader, epochs)
 
+model = Classifier_DropOut()
 
-# Test out your network!
+criterion = torch.nn.NLLLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.003)
 
-dataiter = iter(testloader)
-images, labels = dataiter.next()
-img = images[1].view(1,-1)
+train_losses_dp, test_losses_dp, test_accuracy_dp = train_model(model, optimizer, criterion, trainloader, testloader, epochs)
 
-# TODO: Calculate the class probabilities (softmax) for img
-# Turn off gradients to speed up this part
-with torch.no_grad():
-    logps = model.forward(img) # prediction fase
-
-ps = torch.exp(logps) #with the exponential ps contains the probability for each class
-print(ps)
-#----------
-
-#accuracy calculation on the test set
-with torch.no_grad():
-    logps = model.forward(images.view(images.shape[0],-1))
-ps = torch.exp(logps)
-top_ps, top_classes= ps.topk(1)
-
-accuracy=torch.mean((top_classes==labels.view(*top_classes.shape)).type(torch.FloatTensor))
-
-print(accuracy)
-
+print('end')
